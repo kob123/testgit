fa06dd1971f4aa990e7f4cbfa22b6feb24ce6297
Kevin O'Brien <kevinphillipobrien@kevins-mbp.home>
2017-11-13 10:56:52 +0000

    hello world

--- gittestapp2/ViewController.swift
+++ gittestapp2/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    print("Hello World")
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
