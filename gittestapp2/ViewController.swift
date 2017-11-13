2a10fa8c787b6acaac8e5e6d82a7ce1c8a611af9
Kevin O'Brien <kevinphillipobrien@broadband.bt.com>
2017-11-13 10:50:26 +0000

    Initial Commit

---
+++ gittestapp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittestapp2
+//
+//  Created by Kevin O'Brien on 2017-11-13.
+//  Copyright © 2017 Kevin O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
